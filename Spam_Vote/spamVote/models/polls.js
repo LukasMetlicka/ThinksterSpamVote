95a2f406fa65c7ccac3092b5ce73c9bcd28e8b34
Lukas Metlicka <lmetlicka@g.harvard.edu>
2015-06-07 12:14:17 -0400

    MongoDB models and routes working correctly

--- Spam_Vote/spamVote/models/polls.js
+++ Spam_Vote/spamVote/models/polls.js
@@ -6,6 +6,16 @@ var PollsSchema = new mongoose.Schema({
     redVotes: {type: Number, default: 0},
     blueVotes: {type: Number, default: 0}
 
-})
+});
 
-mongoose.model("Polls", PollsSchema);
+PollsSchema.methods.redUpvote = function(cb){
+    this.redVotes++;
+    this.save(cb);
+};
+
+PollsSchema.methods.blueUpvote = function(cb){
+    this.blueVotes++;
+    this.save(cb);
+};
+
+mongoose.model("Poll", PollsSchema);
--- Spam_Vote/spamVote/routes/index.js
+++ Spam_Vote/spamVote/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require("mongoose");
 var router = express.Router();
 
 /* GET home page. */
@@ -7,3 +8,53 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+var Poll = mongoose.model("Poll");
+
+router.get('/showPolls', function(req, res, next) {
+    Poll.find(function(err, polls) {
+        if(err){ return next(err); }
+        res.json(polls);
+    });
+});
+
+router.post("/showPolls", function(req, res, next) {
+    var poll = new Poll(req.body);
+    poll.save(function(err, poll){
+        if(err){ next(err); }
+        res.json(poll);
+    })
+});
+
+router.param('poll', function(req, res, next, id){
+   var query = Poll.findById(id);
+
+    query.exec(function(err, poll){
+        if(err){ next(err); }
+        if(!poll){ next(new Error("Can't find Poll!")); }
+
+        req.poll = poll;
+        return next();
+    });
+
+
+});
+
+router.put('/poll/:poll/redUpvote', function(req, res, next){
+   req.poll.redUpvote(function(err, poll){
+        if(err){ return next(err); }
+       res.json(poll);
+   });
+});
+
+router.put('/poll/:poll/blueUpvote', function(req, res, next){
+    req.poll.blueUpvote(function(err, poll){
+        if(err){ return next(err); }
+        res.json(poll);
+    });
+});
+
+//Tester
+router.get('/showPolls/:poll', function(req, res){
+    res.json(req.poll);
+});
